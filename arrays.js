05a1be7a9b943723ec30a31c1376ae11f58f0231
John Wypick <johnwypick@gmail.com>
2018-02-14 15:22:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  array.push
+   array.unshift(element);
   
 }
 
+
+
 function destructivelyAddElementToBeginningOfArray(array, element) {
+   array.unshift(element);
   
 }
+  var destructivelyAddElementToBeginningOfArray = (array,element);
+    destructivelyAddElementToBeginningOfArray.unshift(array, element);
 
 
